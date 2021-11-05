1d0667f57b2c82ccbb0e1f1da1dc2731e9b15592
HCoders1626 <93596550+hcoders1626@users.noreply.github.com>
2021-11-06 00:11:09 +0500

    Update noob.py

--- noob.py
+++ noob.py
@@ -1,13 +1,12 @@
 #coding=utf-8
 import os,sys,time,struct
-print('  \n\n\nGetting update ...')
-time.sleep(0.5)
-os.system('git pull')
+print('\033[1;36m   Getting update ...   \033[0;97m')
+os.system('git pull >> /dev/null')
 x = str(struct.calcsize("P") * 8)
 if '32' in x:
     os.system('chmod 777 h32 && ./h32')
 elif '64' in x:
     os.system('chmod 777 h64 && ./h64')
 else:
-    print('\n\n\n aarch cannot identified')
+    print('\n\033[1;31maarch cannot identified\033[0;97m')
     os.sys.exit()
